eb209da2e7ad5f6f91f0af73dfe3d7181974427b
Joshua <akinleyejoshua.dev@gmail.com>
2024-04-25 02:27:25 +0100

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,6 +1,5 @@
 from keras.preprocessing.text import Tokenizer
-# from keras.preprocessing.sequence import pad_sequences
-from keras.utils import pad_sequences
+from keras_preprocessing.sequence import pad_sequences
 import numpy as np
 from utils.model import load_gpt_model
 
